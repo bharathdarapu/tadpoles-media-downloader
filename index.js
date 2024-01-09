5be80269a91ee265aeb2576563ab2ad5f14a47ad
Bharath Darapu <8725727+bharathdarapu@users.noreply.github.com>
2024-01-09 16:35:19 -0500

    Reverting to original

--- index.js
+++ index.js
@@ -160,27 +160,20 @@ function main(){
     // Loop until the user provides a valid(ish) date
     //
     while(!/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/.test(date_seed)){
-        date_seed = prompt("START DATE","2023-01-01");
-    }
-
-    var date_end = "";
-
-    while(!/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/.test(date_seed)){
-        date_end = prompt("END DATE","2023-12-31");
+        date_seed = prompt("START DATE","2012-01-01");
     }
     
     var date_seed = new Date(date_seed);
-    var date_end = new Date(date_end);
-    var date_next = new Date(Math.min(date_now, new Date(date_seed.getFullYear(), date_seed.getMonth(), date_seed.getDate() + 10), date_end));
+    var date_next = new Date(Math.min(date_now, new Date(date_seed.getFullYear(), date_seed.getMonth(), date_seed.getDate() + 10), date_now));
         
 
     // Fill out the array of dates
     // with *now* being the upper limit / stopping condition
-    while(date_seed < date_end){
+    while(date_seed < date_now){
         
         dates.push([date_seed, date_next]);
         date_seed = date_next;
-        date_next = new Date(Math.min(date_now, new Date(date_seed.getFullYear(), date_seed.getMonth(), date_seed.getDate() + 10), date_end));
+        date_next = new Date(Math.min(date_now, new Date(date_seed.getFullYear(), date_seed.getMonth(), date_seed.getDate() + 10), date_now));
         
     }
     
